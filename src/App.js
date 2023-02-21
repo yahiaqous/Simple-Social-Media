98e01b5e956dc7dcd89058d5857234e50bbe7a84
YAHIAQOUS <yahiaqous@gmail.com>
2023-02-21 20:16:42 +0300

    Created the Auth Page and Pages Routes in the App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 /** @format */
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Box, Toolbar } from '@mui/material';
 
 import Header from './components/Header';
 import Home from './main_pages/Home';
+import Auth from './main_pages/Auth';
 
 export default function App() {
   return (
-    <Box>
-      <Header />
+    <Router>
+      <Box>
+        {/* Header */}
+        <Header />
 
-      <Box sx={{ p: 3 }}>
-        <Toolbar />
-        <Home />
+        <Box sx={{ p: 3 }}>
+          <Toolbar />
+
+          {/* Main Pages */}
+          <Routes>
+            <Route exact path='/' element={<Home />}></Route>
+            <Route exact path='/auth' element={<Auth />}></Route>
+          </Routes>
+        </Box>
       </Box>
-    </Box>
+    </Router>
   );
 }
